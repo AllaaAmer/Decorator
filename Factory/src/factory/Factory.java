5e0bbd9f66b1710e48e614a1d1614ea0ce8e8496
AllaaAmer <lolitabadr94@gmail.com>
2017-02-14 15:10:57 +0200

    last one

--- Factory/src/factory/Factory.java
+++ Factory/src/factory/Factory.java
@@ -15,13 +15,27 @@ public class Factory {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+
+        PizzaFactory factory = new PizzaFactory();
         //********************AlaaStart**********************
+        String type = "Cheese";
+        PizzaRestaurant rest = new PizzaRestaurant(factory);
+        rest.OrderPizza(type);
         
+        System.out.println("************************************************");
+
+        String type1 = "Vegetables";
+        PizzaRestaurant rest1 = new PizzaRestaurant(factory);
+        rest.OrderPizza(type1);
+        
+        System.out.println("************************************************");
+
         //********************AlaaEnd**********************
         //********************NourStart**********************
-        
+        String type2 = "SeaFood";
+        PizzaRestaurant rest2 = new PizzaRestaurant(factory);
+        rest.OrderPizza(type2);
         //********************NourEnd**********************
     }
-    
+
 }
