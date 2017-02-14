fc7a612bcb66fcfa7753b8c6a56d23c8db93b4aa
Nour <nour.khaled.kembal@hotmail.com>
2017-02-14 14:39:26 +0200

    factory commit2

--- Factory/src/factory/Factory.java
+++ Factory/src/factory/Factory.java
@@ -3,18 +3,19 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Factory;
+package factory;
 
 /**
  *
  * @author Nour
  */
-public class NewMain {
+public class Factory {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
         //********************AlaaStart**********************
         
         //********************AlaaEnd**********************
