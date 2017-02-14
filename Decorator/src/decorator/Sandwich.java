697e3a9b50fd6eb5c2cd0ee524574db69e46b022
AllaaAmer <lolitabadr94@gmail.com>
2017-02-14 13:25:08 +0200

    first commit

---
+++ Decorator/src/decorator/Sandwich.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package decorator;
+
+/**
+ *
+ * @author alaa
+ */
+public class Sandwich {
+    
+    int price;
+    String description;
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+      
+    
+}
